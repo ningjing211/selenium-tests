a9a7a3bdf2ce0f5ec189d968286c251542f4316e
Flow <guanjun211@gmail.com>
2024-07-03 16:28:26 +0800

    weird version but can pass second account loggin

--- existedMemberPurchase.js
+++ existedMemberPurchase.js
@@ -50,7 +50,7 @@ const rl = readline.createInterface({
 
         // 打開 WordPress 登入頁面
         await driver.get('https://www.energyheart.com.tw/wp-login.php');
-        console.log('已打開登入頁面');
+        console.log('Admin檢查此帳號客戶角色....');
         await takeScreenshot('Admin-已打開登入頁面.png');
 
         // 等待登入表單元素顯示
@@ -69,7 +69,7 @@ const rl = readline.createInterface({
 
         // 等待登入完成，檢查是否進入後台首頁
         await driver.wait(until.urlContains('wp-admin'), 30000);
-        await takeScreenshot('Admin-登入成功進入後台.png');
+        await takeScreenshot('Admin-成功登入後台.png');
 
         // 切換到使用者頁面，準備查看登入者帳號的使用者角色(經銷商等級)
         await driver.get('https://www.energyheart.com.tw/wp-admin/users.php');
@@ -83,110 +83,118 @@ const rl = readline.createInterface({
         await driver.sleep(7000); // 假設等待 7 秒鐘
         
         const tdValue = await driver.findElement(By.xpath('//td[@class="role column-role" and @data-colname="使用者角色"]')).getText();
-        console.log('使用者角色:', tdValue);
+        console.log(`${user_account}的使用者角色為:`, tdValue);
 
+        // 打開 WordPress 登入頁面
+        await driver.get('https://www.energyheart.com.tw/wp-login.php');
+        console.log('1-已打開登入頁面');
+        await takeScreenshot('1-已打開登入頁面.png');
 
-    //     // 打開 WordPress 登入頁面
-    //     await driver.get('https://www.energyheart.com.tw/wp-login.php');
-    //     console.log('1-已打開登入頁面');
-    //     await takeScreenshot('1-已打開登入頁面.png');
+        // 等待登入表單元素顯示
+        await driver.wait(until.elementLocated(By.id('user_login')), 10000);
 
-    //     // 等待登入表單元素顯示
-    //     await driver.wait(until.elementLocated(By.id('user_login')), 15000);
+        // 填寫登入表單
+        const userAccount = user_account;  // 替換為管理員用戶名
+        const userPassword = user_password;  // 替換為管理員密碼
+        // console.log(userAccount, userPassword);
+        // 使用 JavaScript 設置輸入框的值
 
-    //     // 填寫登入表單
-    //     const userAccount = user_account;  // 替換為管理員用戶名
-    //     const userPassword = user_password;  // 替換為管理員密碼
-    //     await driver.findElement(By.id('user_login')).sendKeys(userAccount);
-    //     await driver.findElement(By.id('user_pass')).sendKeys(userPassword);
-    //     console.log('2-已填寫登入表單');
-    //     await takeScreenshot('2-已填寫登入表單.png');
+        await takeScreenshot('aaa.png');
+        await driver.executeScript("document.getElementById('user_login').setAttribute('value', arguments[0]);", userAccount);
+        await takeScreenshot('bbb.png');
+        await driver.findElement(By.id('user_pass')).sendKeys(userPassword);
+        await takeScreenshot('ccc.png');
+        console.log('2-已填寫登入表單');
+        await takeScreenshot('2-已填寫登入表單.png');
 
-    //     // 提交登入表單
-    //     await driver.findElement(By.id('wp-submit')).click();
-    //     console.log('3-已提交登入表單');
+        // 提交登入表單
+        console.log('3-提交登入表單前(about to click)');
+        await driver.findElement(By.id('wp-submit')).click();
+        await takeScreenshot('ddd.png');
+        console.log('3-已提交登入表單(after click)');
+        await takeScreenshot('eee.png');
 
-    //     // 等待登入完成，檢查是否進入後台首頁
-    //     await driver.sleep(5000); // 假設等待 5 秒鐘
-    //     console.log('4-登入後進到的地方(管理員會到後台/一般使用者到my-account');
-    //     await takeScreenshot('4-登入後進到的地方.png');
+        // 等待登入完成，檢查是否進入後台首頁
+        await driver.sleep(5000); // 假設等待 5 秒鐘
+        console.log('4-登入後進到的地方(管理員會到後台/一般使用者到my-account');
+        await takeScreenshot('4-登入後進到的地方.png');
 
-    //     // 購物流程
-    //     // 進入購物頁面 https://www.energyheart.com.tw/products/
-    //     await driver.get('https://www.energyheart.com.tw/products/');
-    //     console.log('5-1已進入購物頁面');
+        // 購物流程
+        // 進入購物頁面 https://www.energyheart.com.tw/products/
+        await driver.get('https://www.energyheart.com.tw/products/');
+        console.log('5-1已進入購物頁面');
 
-    //     // 點擊「新航域合作專區」
-    //     await driver.findElement(By.xpath('//div[contains(text(), "新航域合作專區")]')).click();
-    //     console.log('5-2已點擊「新航域合作專區」');
-    //     await takeScreenshot('5-2進入新航域合作專區.png');  // 截圖選擇專區
+        // 點擊「新航域合作專區」
+        await driver.findElement(By.xpath('//div[contains(text(), "新航域合作專區")]')).click();
+        console.log('5-2已點擊「新航域合作專區」');
+        await takeScreenshot('5-2進入新航域合作專區.png');  // 截圖選擇專區
         
-    //     // 選擇購買「....艙」
+        // 選擇購買「....艙」
         
-    //     const comboBuying = await new Promise((resolve) => {
-    //         rl.question('您要買的套餐?(請輸入:豪華經典艙/能量商務艙/尊貴頭等艙):', resolve);
-    //     });
+        const comboBuying = await new Promise((resolve) => {
+            rl.question('您要買的套餐?(請輸入:豪華經典艙/能量商務艙/尊貴頭等艙):', resolve);
+        });
 
-    //     const comboName = comboBuying.trim();
-    //     console.log('6-1您輸入的指定套餐為:', comboName);
+        const comboName = comboBuying.trim();
+        console.log('6-1您輸入的指定套餐為:', comboName);
 
-    //     await driver.findElement(By.xpath(`//h2[contains(text(), "${comboName}")]`)).click();
-    //     console.log(`6-2已選擇「${comboName}」`);
-    //     await takeScreenshot(`6-2已選擇${comboName}.png`);  // 截圖選擇產品
+        await driver.findElement(By.xpath(`//h2[contains(text(), "${comboName}")]`)).click();
+        console.log(`6-2已選擇「${comboName}」`);
+        await takeScreenshot(`6-2已選擇${comboName}.png`);  // 截圖選擇產品
 
-    //     rl.close();
+        rl.close();
 
-    //    // 加入購物車
-    //     let addToCartButton = await driver.findElement(By.css('.single_add_to_cart_button'));
-    //     await driver.wait(until.elementIsVisible(addToCartButton), 10000); // 等待元素可見
-    //     await addToCartButton.click();
-    //     await takeScreenshot('7-1-點選加入購物車.png');  // 截圖選擇產品
-    //     console.log('7-1已加入購物車');
-    //     await driver.sleep(5000); // 假設等待 5 秒鐘
-    //     await takeScreenshot('7-2-可以查看購物車了.png');  // 截圖選擇產品
+       // 加入購物車
+        let addToCartButton = await driver.findElement(By.css('.single_add_to_cart_button'));
+        await driver.wait(until.elementIsVisible(addToCartButton), 10000); // 等待元素可見
+        await addToCartButton.click();
+        await takeScreenshot('7-1-點選加入購物車.png');  // 截圖選擇產品
+        console.log('7-1已加入購物車');
+        await driver.sleep(5000); // 假設等待 5 秒鐘
+        await takeScreenshot('7-2-可以查看購物車了.png');  // 截圖選擇產品
         
-    //     // 進入購物車頁面
-    //     await driver.get('https://www.energyheart.com.tw/checkout-2/');
-    //     console.log('8-1已進入購物車頁面');
-    //     await takeScreenshot('8-1-進入購物車頁面.png');
+        // 進入購物車頁面
+        await driver.get('https://www.energyheart.com.tw/checkout-2/');
+        console.log('8-1已進入購物車頁面');
+        await takeScreenshot('8-1-進入購物車頁面.png');
 
-    //     // 假設這裡需要進行結帳的相關操作，例如選擇配送方式、填寫地址等
-    //     await driver.sleep(7000);
-    //     await takeScreenshot('8-2-讓購物車頁面跳轉後.png');
-    //     console.log('8-2購物車頁面跳轉了');
+        // 假設這裡需要進行結帳的相關操作，例如選擇配送方式、填寫地址等
+        await driver.sleep(7000);
+        await takeScreenshot('8-2-讓購物車頁面跳轉後.png');
+        console.log('8-2購物車頁面跳轉了');
 
-    //     const testFirstName = '姓' + user_account;
-    //     const testLastName = '名' + user_account;
-    //     const testPhone ='0966520168';
-    //     await driver.findElement(By.name('billing_first_name')).sendKeys(testFirstName);
-    //     // await takeScreenshot('91-selected_product.png');
-    //     await driver.findElement(By.name('billing_last_name')).sendKeys(testLastName);
-    //     // await takeScreenshot('92-selected_product.png');
-    //     await driver.findElement(By.name('billing_phone')).sendKeys(testPhone);
-    //     await takeScreenshot('9-填完姓名電話後.png');
-    //     console.log('9-填完姓名電話後');
+        const testFirstName = '姓' + user_account;
+        const testLastName = '名' + user_account;
+        const testPhone ='0966520168';
+        await driver.findElement(By.name('billing_first_name')).sendKeys(testFirstName);
+        // await takeScreenshot('91-selected_product.png');
+        await driver.findElement(By.name('billing_last_name')).sendKeys(testLastName);
+        // await takeScreenshot('92-selected_product.png');
+        await driver.findElement(By.name('billing_phone')).sendKeys(testPhone);
+        await takeScreenshot('9-填完姓名電話後.png');
+        console.log('9-填完姓名電話後');
 
-    //     await driver.findElement(By.name('woocommerce_checkout_place_order')).click();
-    //     console.log('9-1點選結帳按鈕, 準備並等待跳轉');
-    //     await driver.sleep(15000);
-    //     console.log('9-2跳轉成功-完成結帳-顯示訂單頁面');
-    //     await takeScreenshot('9-2完成結帳-顯示訂單頁面.png');
-    //     // 查找 <li> 标签中的 <strong> 子标签
-    //     const strongElement = await driver.findElement(By.css('.woocommerce-order-overview__order.order strong'));
-    //     const orderNumber = await strongElement.getText();
-    //     console.log('10-取得訂單編號-', orderNumber);
+        await driver.findElement(By.name('woocommerce_checkout_place_order')).click();
+        console.log('9-1點選結帳按鈕, 準備並等待跳轉');
+        await driver.sleep(15000);
+        console.log('9-2跳轉成功-完成結帳-顯示訂單頁面');
+        await takeScreenshot('9-2完成結帳-顯示訂單頁面.png');
+        // 查找 <li> 标签中的 <strong> 子标签
+        const strongElement = await driver.findElement(By.css('.woocommerce-order-overview__order.order strong'));
+        const orderNumber = await strongElement.getText();
+        console.log('10-取得訂單編號-', orderNumber);
 
-    //     // 將結果保存到Excel
-    //     const workbook = xlsx.utils.book_new();
-    //     const worksheetData = [
-    //     ['User Email', 'User Level', 'Purchased combo', 'orderNumber'],
-    //     [user_account, userLevel, comboName, orderNumber, /* more data here */]
-    //     ];
-    //     const worksheet = xlsx.utils.aoa_to_sheet(worksheetData);
-    //     xlsx.utils.book_append_sheet(workbook, worksheet, 'Test Results');
-    //     xlsx.writeFile(workbook, 'purchased_results.xlsx');
-    //     console.log('寫入Excel檔案並輸出:', `${user_account}_purchased_results.xlsx`);
-    //     console.log('完成結帳流程');
+        // 將結果保存到Excel
+        const workbook = xlsx.utils.book_new();
+        const worksheetData = [
+        ['User Email', 'User Level', 'Purchased combo', 'orderNumber'],
+        [user_account, userLevel, comboName, orderNumber, /* more data here */]
+        ];
+        const worksheet = xlsx.utils.aoa_to_sheet(worksheetData);
+        xlsx.utils.book_append_sheet(workbook, worksheet, 'Test Results');
+        xlsx.writeFile(workbook, 'purchased_results.xlsx');
+        console.log('寫入Excel檔案並輸出:', `${user_account}_purchased_results.xlsx`);
+        console.log('完成結帳流程');
     } finally {
         // 關閉瀏覽器
         await driver.quit();
