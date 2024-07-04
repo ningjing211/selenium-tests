99761efa9f94554a7684bd79404eab471192fc49
Flow <guanjun211@gmail.com>
2024-07-04 18:24:08 +0800

    admin新增寫入經銷商等級功能

--- admin.js
+++ admin.js
@@ -13,7 +13,7 @@ const rl = readline.createInterface({
 
 (async function reviewOrder() {
     let options = new chrome.Options();
-    options.addArguments('--headless');  // 設置無頭模式
+    // options.addArguments('--headless');  // 設置無頭模式
     options.addArguments('--no-sandbox');
     options.addArguments('--disable-dev-shm-usage');
 
@@ -46,17 +46,36 @@ const rl = readline.createInterface({
 
             // 假設訂單編號在第二行第四列（A1, B1, C1, D1）
             const orderNumber = data[1][3]; // [1] 是第二行，[3] 是第四列
-            return orderNumber;
+            const user_email = data[1][0];
+            return {orderNumber, user_email};
         } catch (error) {
             console.error('Error reading Excel file:', error);
             return null;
         }
     }
 
+    function reflectUserLevel (combo){
+        switch (combo) {
+            case "尊貴頭等艙":
+                return "A級經銷商";
+            case "能量商務艙":
+                return "B級經銷商";
+            case "豪華經典艙":
+                return "C級經銷商";
+            default:
+                return "未知"; // 或者可以根據需要返回其他預設值或處理方式
+        }
+    }
+
     try {
         console.log('準備讀取Excel裡面的購買訂單號碼...');
         const filename = '18793@surfman.com_purchased_results.xlsx';
-        const orderNumber = getOrderNumberFromExcel(filename);
+        const result = getOrderNumberFromExcel(filename);
+
+        const orderNumber = result.orderNumber;
+        const userEmail = result.user_email;
+
+        console.log('成功讀取到此帳號Email, Email:', userEmail);
         console.log('成功讀取到訂單號碼, Order Number:', orderNumber);
 
         const getOrderID = await new Promise((resolve) => {
@@ -155,6 +174,56 @@ const rl = readline.createInterface({
         await driver.findElement(By.id('doaction')).click();
         console.log('10-已執行更改訂單狀態動作');
         await takeScreenshot('10-已執行更改訂單狀態動作.png');
+
+        // 前往使用者管理頁面
+        //查看使用者User Level經銷商變化與否
+        let orderLink = `https://www.energyheart.com.tw/wp-admin/admin.php?page=wc-orders&action=edit&id=${orderNumber}`;
+        console.log('orderLink:', orderLink);
+        await driver.get(orderLink);
+        await driver.sleep(2000); // 添加延遲以確保上一步操作完成
+
+        const element = await driver.findElement(By.xpath("//tbody[@id='order_line_items']//a[@class='wc-order-item-name']"));
+        let boughtComboPaid = await element.getText();
+
+        console.log(boughtComboPaid);
+        console.log('11-1-已進入使用者管理頁面');
+        await takeScreenshot('11-1-已進入使用者管理頁面.png');
+
+        let currentLevel = reflectUserLevel(boughtComboPaid);
+        console.log(currentLevel);
+
+        // 因為付款成功了, 所以打開檔案寫入經銷商的等級
+        // 檔案路徑
+        const filePath = filename; // 替換為你的實際檔案路徑
+
+        // 讀取 Excel 檔案
+        const workbook = xlsx.readFile(filePath);
+
+        // 取得第一個工作表
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+
+        // 將工作表轉換為 JSON 格式
+        const data = xlsx.utils.sheet_to_json(worksheet, { header: 1 });
+        console.log(data);
+        // 取得 user_email 的值（假設在第二行第二列）
+        let user_level = data[1][1]; // 假設 user_level 在第二行第二列（A2, B2, C2, D2...）
+        console.log(user_level);
+        // 做一些處理後更新 user_level，這裡只是示範，你可以根據你的需求更新它
+        user_level = currentLevel; // 假設要更新成這個新的 email
+
+        // 將更新後的 user_level 放回原始的資料中
+        data[1][1] = user_level;
+        console.log(data[1][1]);
+        // 將更新後的資料寫回 Excel 檔案
+        const updatedSheet = xlsx.utils.aoa_to_sheet(data);
+        workbook.Sheets[sheetName] = updatedSheet;
+
+        // 將更新後的 Excel 檔案寫入到磁碟
+        xlsx.writeFile(workbook, filePath);
+
+        console.log('已將 user_email 更新到原始的 Excel 檔案中:', filePath);
+
 
     } finally {
         // 關閉瀏覽器
